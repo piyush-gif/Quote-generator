12aa6d8b354ab16dfb7e7876184fca32b8070676
piyush <piyush.khadka2233@gmail.com>
2025-06-02 19:57:50 +0100

    added necessary buttons to fetch the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quote, setQuote] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const handleQuote =() =>{
+    setLoading(true);
+    fetch('https://api.quotable.io/random',{})
+    .then((res) => {
+      if (!res){
+        throw Error;
+      }
+      return res.json();
+    })
+    .then((data) => {
+      setQuote(data);
+      console.log(data);
+    })
+    .catch((e) => {
+      setError(true);
+    })
 
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="quote-container">
+      <h3>Quote Generator</h3>
+      <div className="quote"> 
+        {quote && <p>{quote}</p>}
+        <button onClick={()=> {handleQuote}}>New Quote</button>
+        <button>copy</button>
+        <button>X</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
